cc149bd130a40cbe1307f8faae97d9112f75816e
rigole <foplacide@gmail.com>
2022-05-26 11:02:02 +0100

    creating the Header and Footer component and render them into the App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-
 import './App.css';
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+         <Header/>
+            <main>
+                <h1>Hello and Welcome</h1>
+            </main>
+        <Footer/>
     </div>
   );
 }
