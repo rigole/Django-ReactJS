d35941e42319a5ac1b24ca2b7fb7b1b553a5f7e4
rigole <foplacide@gmail.com>
2022-05-28 15:24:20 +0100

    Screen page with static product 1

--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import Rating from '../components/Rating'
+import products from "../products";
 
 function ProductScreen(){
-
+    const { id } = useParams()
+    const product = products.find((p) => p._id === (id))
     return(
         <div>
-            Product
+           <Link to="/" className="btn btn-light my-3">Go Back</Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid/>
+                </Col>
+
+                <Col md={3}>
+                    <ListGroup>
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text={`${product.numReviews} Rating`} color={'#f8e825'}/>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+
+                </Col>
+            </Row>
         </div>
     )
 }
