30b2d41320f37855fc382e5fd6027ed671517870
rigole <foplacide@gmail.com>
2022-05-28 16:02:40 +0100

    Screen page with static product 2

--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -16,7 +16,7 @@ function ProductScreen(){
                 </Col>
 
                 <Col md={3}>
-                    <ListGroup>
+                    <ListGroup variant="flush">
                         <ListGroup.Item>
                             <h3>{product.name}</h3>
                         </ListGroup.Item>
@@ -24,10 +24,42 @@ function ProductScreen(){
                         <ListGroup.Item>
                             <Rating value={product.rating} text={`${product.numReviews} Rating`} color={'#f8e825'}/>
                         </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            Price: ${product.price}
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            Description: {product.description}
+                        </ListGroup.Item>
                     </ListGroup>
                 </Col>
                 <Col md={3}>
+                    <Card>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
 
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>
+                                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Button className="btn-block" disabled={product.countInStock == 0} type="button">Add to Cart</Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
                 </Col>
             </Row>
         </div>
