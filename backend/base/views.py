1b7fd5f946378e9d59c22bb93aeb69818129578d
rigole <foplacide@gmail.com>
2022-05-29 16:47:18 +0100

    setting the route in the backend

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+
+def getRoutes(request):
+   routes = [
+       '/api/products/',
+       '/api/products/create/',
+       
+       '/api/products/upload/',
+       'api/products/<id>/reviews/',
+       
+       '/api/products/top/',
+       '/api/products/<id>/',
+       
+       '/api/products/delete/<id>/',
+       '/api/products/<update>/<id>'
+   ]
+   return JsonResponse('Hello', safe=False)
+
