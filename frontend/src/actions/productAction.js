507851f59be99d19d1cdd062f7e16db798e2e96a
rigole <foplacide@gmail.com>
2022-06-04 15:50:01 +0100

    setting the action

---
+++ frontend/src/actions/productAction.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import {
+    PRODUCT_LIST_FAIL,
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS
+} from "../constants/productConstants";
+
+const listProducts = () => async (dispatch) => {
+  try {
+      dispatch({ type: PRODUCT_LIST_REQUEST })
+
+      const { data } =  axios.get('/api/products/')
+
+      dispatch({
+          type: PRODUCT_LIST_SUCCESS,
+          payload: data
+      })
+
+  } catch (error){
+        dispatch({
+            type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+
+        })
+  }
+}
