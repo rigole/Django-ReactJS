7e2ee7bfb4256182747f32a99d1bfd472bb8834f
rigole <foplacide@gmail.com>
2022-06-14 16:37:10 +0100

    Cart Screen setting 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ function App() {
                         <Route path='/' element={<HomeComponent/>} exact/>
                         <Route path="/product/:id" element={<ProductScreen/>} />
                         <Route path="/cart/:id" element={<CartScreen/>}/>
+                        <Route path="/cart" element={<CartScreen/>}/>
                     </Routes>
                 </Container>
             </main>
