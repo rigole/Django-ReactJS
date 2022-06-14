40e5b795510309c30c46f54ec7a49607cd20c843
rigole <foplacide@gmail.com>
2022-06-14 21:56:22 +0100

    Cart Screen setting disposal of product once they are in the cart

--- frontend/src/screen/CartComponent.js
+++ frontend/src/screen/CartComponent.js
@@ -48,10 +48,40 @@ function CartScreen({ match, location }){
                             <ListGroup.Item key={item.product}>
                                 <Row>
                                     <Col md={2}>
-                                        <Image src={item.image} />
+                                        <Image src={item.image} alt={item.name} fluid rounded/>
                                     </Col>
                                     <Col md={3}>
-                                        <Link to={``}>{item.name}</Link>
+                                        <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                    </Col>
+
+                                    <Col md={2}>
+                                        ${item.price}
+                                    </Col>
+
+                                    <Col md={3}>
+                                        <Form.Control
+                                            as="select"
+                                            value={item.qty}
+                                            onChange={(e) => dispatch(addToCart(item.product, e.target.value))}
+                                        >
+                                            {
+                                                [...Array(item.countInStock).keys()].map((x) =>(
+                                                    <option key={x + 1} value={x + 1}>
+                                                        {x + 1}
+                                                    </option>
+                                                ))
+
+                                            }
+                                        </Form.Control>
+
+                                    </Col>
+                                    <Col md={1}>
+                                        <Button
+                                            type="button"
+                                            variant="light"
+                                        >
+                                            <i className="fas fa-trash"></i>
+                                        </Button>
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
