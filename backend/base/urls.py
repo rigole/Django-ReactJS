5f63bb44a19fd2409011b70ab10f9bbc08507116
rigole <foplacide@gmail.com>
2022-06-18 09:39:21 +0100

    customizing the JWT

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
+"""
+    from rest_framework_simplejwt.views import (
+        TokenObtainPairView,
+    )
+"""
 
 urlpatterns = [
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="routes"),
     path('products/<str:pk>/', views.getProduct, name="product")
