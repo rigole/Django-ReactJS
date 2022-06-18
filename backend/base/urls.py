b45ca64d8d1890754a241cab95f3e73e35aff5a9
rigole <foplacide@gmail.com>
2022-06-18 08:13:35 +0100

    adding the simple JWT to settings.py and testing it

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('api/users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="routes"),
     path('products/<str:pk>/', views.getProduct, name="product")
