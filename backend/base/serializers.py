9e97fb37ac628642b21437a1ebf9750f36185f97
rigole <foplacide@gmail.com>
2022-06-19 22:47:11 +0100

    get users infos suing token and postman

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -13,4 +13,4 @@ class ProductSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'email']
