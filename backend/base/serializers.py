1aaee29daacf638a3bc33dcb150f49f568c9d924
rigole <foplacide@gmail.com>
2022-06-21 20:44:03 +0100

    serializing and returning token

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -3,6 +3,20 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Product
 
+
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
+
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
@@ -13,6 +27,8 @@ class ProductSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField(read_only=True)
     _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = User
         fields = ['id', '_id', 'username', 'email', 'name']
@@ -22,8 +38,12 @@ class UserSerializer(serializers.ModelSerializer):
         if name == '':
             name = obj.email
 
+        return name
+
 
     def get__id(self, obj):
         return obj.id
 
-        return name
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
