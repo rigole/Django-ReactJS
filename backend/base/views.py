01a179e3c611f6b27109356cbd1df23e2fe32203
rigole <foplacide@gmail.com>
2022-06-22 21:27:00 +0100

    setting routes and permissions

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,12 @@
 from itertools import product
+import django
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
+from django.contrib.auth.models import User
 from .models import Product
 from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
@@ -50,6 +53,14 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+    
+
+@api_view(['GET'])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
@@ -57,6 +68,7 @@ def getUserProfile(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
