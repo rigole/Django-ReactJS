3dbc6d69325f93cfb3c20659f51044f1b8c7a3d6
rigole <foplacide@gmail.com>
2022-07-01 08:47:07 +0100

    userprofile view and urls

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('register/', views.registerUser, name="register"),
     
     path('profile/', views.getUserProfile, name="users-profile"),
+    path('profile/update/', views.updateUserProfile, name="user-profile-update"),
     path('', views.getUsers, name="users-profile"),
 
 ]
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -53,6 +53,25 @@ def registerUser(request):
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
+#Changing profile user
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    
+    data = request.data
+    
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+        
+    user.save()
+    
+    return Response(serializer.data)
+
 
 #get user profile once we arre connected
 @api_view(['GET'])
