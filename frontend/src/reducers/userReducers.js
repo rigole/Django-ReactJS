688737b6a455b8389313425fa84436b17ccd3e85
rigole <foplacide@gmail.com>
2022-07-01 19:17:02 +0100

    Profile component and get user details to change them

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -8,7 +8,12 @@ import {
 
     USER_REGISTER_SUCCESS,
     USER_REGISTER_FAIL,
-    USER_REGISTER_REQUEST
+    USER_REGISTER_REQUEST,
+
+    USER_DETAILS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+
 } from "../constants/userConstants";
 
 
@@ -48,6 +53,24 @@ export const userRegisterReducer = (state = {}, action) => {
 
         case USER_LOGOUT:
             return {}
+
+        default:
+            return state
+
+    }
+}
+
+export const userDetailsReducer = (state = {user: {} }, action) => {
+
+    switch (action.type){
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true }
+
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload }
+
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
 
         default:
             return state
---
+++ frontend/src/screen/ProfileComponent.js
@@ -0,0 +1,130 @@
+import React,  {useEffect, useState} from 'react'
+import {Link, useLocation, useNavigate} from 'react-router-dom'
+import { Form, Button, Row, Col  } from 'react-bootstrap'
+import { useDispatch, useSelector } from "react-redux";
+import Loader from '../components/Loader'
+import Message from "../components/Message";
+import FormContainer from "../components/FormContainer";
+import {getUserDetails, login, register} from "../actions/userActions";
+
+
+function ProfileComponent(){
+
+
+    const location = useLocation()
+    const navigate = useNavigate()
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState('')
+
+    const dispatch = useDispatch()
+
+    //const redirect = location.search ? location.search.split('=')[1] : '/'
+
+    const userDetails = useSelector(state => state.userDetails)
+    const { error, loading, user } = userDetails
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+    useEffect(() => {
+        if(!userInfo){
+            navigate("/login")
+        } else {
+            if(!user || !user.name){
+                dispatch(getUserDetails('profile'))
+            }else{
+                setName(user.name)
+                setEmail(user.email)
+            }
+        }
+    }, [dispatch,navigate, userInfo,user])
+
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if (password !== confirmPassword) {
+            setMessage("Passwords do not match ")
+        } else {
+            console.log("updating")
+        }
+
+
+       // dispatch(register(name, email, password))
+    }
+
+    return (
+        <Row>
+            <Col md={3}>
+                <h1>User Profile</h1>
+
+                {message && <Message variant='danger' message={message}/>}
+                {error && <Message variant="danger" message={error}/>}
+                {loading && <Loader/>}
+                <Form onSubmit={submitHandler}>
+
+                    <Form.Group controlId='name'>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            required
+                            type='text'
+                            placeholder='Enter Name'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        >
+                        </Form.Control>
+                    </Form.Group>
+
+
+                    <Form.Group controlId='email'>
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control
+                            required
+                            type='email'
+                            placeholder='Enter Email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        >
+                        </Form.Control>
+                    </Form.Group>
+
+
+                    <Form.Group controlId='password'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type='password'
+                            placeholder='Enter Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        >
+                        </Form.Control>
+                    </Form.Group>
+
+
+                    <Form.Group controlId='confirmPassword'>
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control
+                            type='password'
+                            placeholder='Confirm Password'
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        >
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Button type="submit" variant="primary">
+                        Update
+                    </Button>
+                </Form>
+            </Col>
+
+            <Col md={9}>
+                <h1>My orders</h1>
+            </Col>
+        </Row>
+    )
+}
+
+export default ProfileComponent
