39178f1165783a2be8dba9193621c98c87aeb9e1
rigole <foplacide@gmail.com>
2022-07-13 13:04:59 +0100

    order details reducer and actions

--- frontend/src/reducers/orderReducer.js
+++ frontend/src/reducers/orderReducer.js
@@ -3,7 +3,11 @@ import {
     ORDER_CREATE_FAIL,
     ORDER_CREATE_REQUEST,
 
-    ORDER_CREATE_RESET
+    ORDER_CREATE_RESET,
+
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_FAIL
 }
     from '../constants/orderConstant'
 
@@ -29,6 +33,32 @@ export const orderCreateReducer = (state = {}, action) => {
             }
 
         case ORDER_CREATE_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        default:
+            return state
+    }
+}
+
+
+export const orderDetailsReducer = (state = {loading:true, orderItems:[], shippingAddress:{}}, action) => {
+    switch (action.type) {
+        case ORDER_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+
+        case ORDER_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                order: action.payload
+            }
+
+        case ORDER_DETAILS_FAIL:
             return {
                 loading: false,
                 error: action.payload
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { productListReducer, productDetailsReducer } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer  } from "./reducers/userReducers";
-import { orderCreateReducer } from "./reducers/orderReducer";
+import { orderCreateReducer, orderDetailsReducer } from "./reducers/orderReducer";
 
 
 const reducer = combineReducers({
@@ -15,7 +15,8 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
-    orderCreate: orderCreateReducer
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
